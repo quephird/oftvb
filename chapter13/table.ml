0508dfe016f5ba03ee69678e4338114f2553c13d
danielle kefford <danielle_kefford@hotmail.com>
2015-01-09 01:14:45 -0500

    Improved solution for times table exercise for chapter 13.

--- chapter13/table.ml
+++ chapter13/table.ml
@@ -1,11 +1,9 @@
 let table n =
-  let a = Array.make n [||] in
-    for i = 0 to (n-1) do
-      let r = Array.make n 0 in
-        for j = 0 to (n-1) do 
-          r.(j) <- (i+1)*(j+1);
-        done;
-        a.(i) <- r;
+  let a = Array.map (fun x -> Array.make n 1) (Array.make n 1) in
+    for i = 1 to n do
+      for j = 1 to n do 
+        a.(i-1).(j-1) <- i*j;
+      done;
     done;
     a
 
